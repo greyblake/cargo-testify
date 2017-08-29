94799cfd2b8b15b4f7b300005ab4871156c21e53
Sergey Potapov <blake131313@gmail.com>
2017-08-29 22:15:51 +0200

    Add docs for Outcome

--- src/outcome.rs
+++ src/outcome.rs
@@ -1,5 +1,13 @@
+/// This enum represents an outcome of attempt to run tests.
+/// It's passed to a notifier in order to display a message to user.
+/// Every invariant contains a text message, that is shown in addition.
 pub enum Outcome<'a> {
+    /// Tests have passed successfully
     TestsPassed(&'a str),
+
+    /// Tests failed
     TestsFailed(&'a str),
+
+    /// Compilation error detected
     CompileError(&'a str)
 }
