2af9f8d403f6c946b2a3b311b500a6c483c70e90
Sergey Potapov <blake131313@gmail.com>
2017-08-30 19:04:36 +0200

    Add project dir to Config

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,39 @@
 use std::time::Duration;
+use std::path::PathBuf;
+
+use errors::*;
 
 pub struct Config {
-    pub ignore_duration: Duration
+    pub ignore_duration: Duration,
+    pub project_dir: PathBuf
 }
 
 pub struct ConfigBuilder {
-    ignore_duration: Duration
+    ignore_duration: Duration,
+    project_dir: Option<PathBuf>
 }
 
 impl ConfigBuilder {
     pub fn new() -> Self {
         Self {
-            ignore_duration: Duration::from_millis(300)
+            ignore_duration: Duration::from_millis(300),
+            project_dir: None
         }
     }
 
-    pub fn build(&self) -> Config {
-        Config {
-            ignore_duration: self.ignore_duration
-        }
+    pub fn project_dir(mut self, dir: PathBuf) -> Self {
+        self.project_dir = Some(dir);
+        self
+    }
+
+    pub fn build(self) -> Result<Config> {
+        let project_dir = self.project_dir.ok_or(ErrorKind::ProjectDirMissing)?;
+
+        let config = Config {
+            ignore_duration: self.ignore_duration,
+            project_dir: project_dir
+        };
+        Ok(config)
     }
 }
 
