d22f93c5ab6838d605052becfe748b2a82d91c1f
Ole Martin Ruud <barskern@outlook.com>
2018-09-16 00:06:31 +0200

    Migrate from clap to structopt
    "Structopt" provides custom derives to parse the commandline arguments
    into a config struct, hence it abstracts away a lot of parsing, which
    makes the code less verbose.
    
    This commit also removes tests from the "reactor.rs" file, which were
    mostly testing features from the "glob" package.

--- src/errors.rs
+++
@@ -1,9 +0,0 @@
-error_chain! {
-    errors {
-        ProjectDirMissing { description("project directory is missing") }
-    }
-
-    foreign_links {
-        PatternError(::glob::PatternError);
-    }
-}
